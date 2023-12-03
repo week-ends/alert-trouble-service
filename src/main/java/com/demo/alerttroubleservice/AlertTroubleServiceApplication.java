bac9216f4a53f5716d091904dc50b888918fc959
문일주 <iljoomoon@gmail.com>
2023-12-04 01:30:37 +0900

    Init Project

---
+++ src/main/java/com/demo/alerttroubleservice/AlertTroubleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.alerttroubleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertTroubleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertTroubleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/alerttroubleservice/AlertTroubleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.alerttroubleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertTroubleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
