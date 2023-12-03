12952d43b2ecadaa7b7b50b782799cab0c23c9fa
week <iljoomoon@gmail.com>
2023-12-04 02:47:17 +0900

    [feat]Add Ping Controller

---
+++ src/main/java/com/demo/alerttroubleservice/application/presentation/PingController.java
@@ -0,0 +1,16 @@
+package com.demo.alerttroubleservice.application.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("pong");
+    }
+}
+
+
---
+++ src/test/java/com/demo/alerttroubleservice/application/presentation/PingControllerTest.java
@@ -0,0 +1,25 @@
+package com.demo.alerttroubleservice.application.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class PingControllerTest {
+
+    @Autowired
+    private PingController pingController;
+
+    @Test
+    public void ping() {
+        ResponseEntity<String> response = pingController.ping();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("pong", response.getBody());
+    }
+}
+
+
